a46cfa73c8a3cb2dee0b839253e10b073ac6e54c
Krishna Kammaje <krishna.kammaje@ingenxtec.com>
2024-05-03 14:34:07 +0000

    dependecies

--- mtx/sidecar/server.js
+++ mtx/sidecar/server.js
@@ -11,9 +11,14 @@ cds.env.mtx.dependencies = [services.destination.xsappname, services.connectivit
 cds.on('served', ()=>{
   const { 'cds.xt.ModelProviderService': mps } = cds.services
   const { 'cds.xt.DeploymentService': ds } = cds.services
+  const {'cds.xt.SaasProvisioningService': sp} = cds.services
 
   ds.after ('subscribe', (_,req) => { 
       return 'https://traininggms-ingenx-dev-gms-mt-approuter.cfapps.eu10-004.hana.ondemand.com';
-   })
+   });
+
+  sp.after('getDependecies', (_,req) => {
+    return [services.destination.xsappname, services.connectivity.xsappname];
+  });
 
 })
